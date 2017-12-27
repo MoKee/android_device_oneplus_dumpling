b82a83fbefe8eb6eedb93def001ae0e948d9d0cf
XiNGRZ <chenxingyu92@gmail.com>
2017-12-28 00:19:58 +0800

    dumpling: Create device tree
    Change-Id: I3e25e352ada54bd039d83eadfc00c509e698cf39

---
+++ mokee.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2017-2018 The MoKee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from dumpling device
+$(call inherit-product, device/oneplus/dumpling/device.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_NAME := mk_dumpling
+PRODUCT_DEVICE := dumpling
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := ONEPLUS A5010
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+TARGET_VENDOR_PRODUCT_NAME := OnePlus5T
+TARGET_VENDOR_DEVICE_NAME := OnePlus5T
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5T PRODUCT_NAME=OnePlus5T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OnePlus/OnePlus5T/OnePlus5T:7.1.1/NMF26X/12122320:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=XiNGRZ
+
+TARGET_VENDOR := oneplus
