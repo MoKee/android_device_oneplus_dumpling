583b21313ee2071d57568daba2b1476d262053c5
XiNGRZ <chenxingyu92@gmail.com>
2018-01-01 19:51:49 +0800

    dumpling: PLATFORM_PATH -> DEVICE_PATH
    Change-Id: I97f1d209f13d5f32efcb16b5e0dd00eb70f959ac

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,14 +25,14 @@
 # Inherit from oneplus msm8998-common
 -include device/oneplus/msm8998-common/BoardConfigCommon.mk
 
-PLATFORM_PATH := device/oneplus/dumpling
+DEVICE_PATH := device/oneplus/dumpling
 
 # Assertions
-TARGET_BOARD_INFO_FILE := $(PLATFORM_PATH)/board-info.txt
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := OnePlus5T,dumpling
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_PATH)/bluetooth/include
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # inherit from the proprietary version
 -include vendor/oneplus/dumpling/BoardConfigVendor.mk
