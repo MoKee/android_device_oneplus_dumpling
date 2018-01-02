55fc3dff2d61e16f49e21ea095381eb64c941b39
XiNGRZ <chenxingyu92@gmail.com>
2018-01-02 22:59:46 +0800

    dumpling: Moved cmaera_config.xml to device
    Change-Id: Idcbff1216e9303c00a4432254db0a25d0c07ced3

--- device.mk
+++ device.mk
@@ -63,6 +63,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
 
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/camera_config.xml:system/etc/camera/camera_config.xml
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     fingerprint.msm8998 \
