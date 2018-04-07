d9b1c6d51e1cd1fe47e934a713b0ab28ef8f1c35
XiNGRZ <chenxingyu92@gmail.com>
2018-04-07 21:08:03 +0800

    dumpling: Force enable nav bar for OnePlus 5T
    Change-Id: Ia876de0d3cb42216523f8fd2dfd7756624fb5b6d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,5 +37,8 @@ TARGET_OTA_ASSERT_DEVICE := OnePlus5T,dumpling
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # inherit from the proprietary version
 -include vendor/oneplus/dumpling/BoardConfigVendor.mk
